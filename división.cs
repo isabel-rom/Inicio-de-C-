f43981a540550032b4845ea47d962a5098b4af49
Isabel Romero <114201548+isabel-rom@users.noreply.github.com>
2023-01-05 17:14:44 -0600

    Add files via upload

---
+++ división.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+  static void Main(string [] args)
+  {
+  int numero1 = 1;
+  int numero2 = 0 ;
+  
+  int divición = numero1 / numero2 ;
+  {
+   Console.WriteLine(divición);
+    Console.ReadKey();
+  }
+}
+}
---
+++ suma.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+  static void Main(string [] args)
+  {
+  int numero1 = 8 ;
+  int numero2 = 12 ;
+  
+  int suma = numero1 + numero2 ;
+  {
+   Console.WriteLine(suma);
+    Console.ReadKey();
+  }
+}
+}
