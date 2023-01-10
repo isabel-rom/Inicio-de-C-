48558cc059ca5af968061182145da0111cd63c1e
Isabel Romero <114201548+isabel-rom@users.noreply.github.com>
2023-01-09 20:03:12 -0600

    Add files via upload

---
+++ ejercicio  1-6.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+  static void Main(string [] args)
+{
+                Console.WriteLine("Ingresa lo solicitado: ");
+                
+                Console.WriteLine("Entra la altura del rectángulo: ");
+				float altura;
+				altura = float.Parse(Console.ReadLine());
+				Console.WriteLine("Entra la anchura del rectángulo:");
+				float anchura;
+				anchura = float.Parse(Console.ReadLine());
+			    Console.WriteLine("area del rectangulo");
+				float area;
+				 Console.WriteLine(altura * anchura);
+			     
+}
+}
